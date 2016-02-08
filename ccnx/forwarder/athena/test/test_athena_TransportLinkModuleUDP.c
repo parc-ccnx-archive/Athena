3836998e191b818f9a946aa082b460a20b0d030b
Kev <kevin.fox@parc.com>
2016-02-08 14:11:14 -0800

    2016 copyrights

--- ccnx/forwarder/athena/test/test_athena_TransportLinkModuleUDP.c
+++ ccnx/forwarder/athena/test/test_athena_TransportLinkModuleUDP.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Xerox Corporation (Xerox)and Palo Alto Research Center (PARC)
+ * Copyright (c) 2015-2016, Xerox Corporation (Xerox)and Palo Alto Research Center (PARC)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,7 +26,7 @@
  */
 /**
  * @author Kevin Fox, Palo Alto Research Center (Xerox PARC)
- * @copyright 2015, Xerox Corporation (Xerox)and Palo Alto Research Center (PARC).  All rights reserved.
+ * @copyright 2015-2016, Xerox Corporation (Xerox)and Palo Alto Research Center (PARC).  All rights reserved.
  */
 #include <config.h>
 #include "../athena_TransportLinkModuleUDP.c"
